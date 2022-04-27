1c665dc37c941d5ffcf169b1078472871b89474f
andres <andresyrg16@gmail.com>
2022-04-27 16:01:06 -0500

    terminada

--- 02-JavaScriptAvanzado-II/homework/homework.js
+++ 02-JavaScriptAvanzado-II/homework/homework.js
@@ -7,6 +7,11 @@ function counter() {
   // ejemplo: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+   var contador = 0;
+   return function(){
+     contador++;
+     return contador;
+   } 
 }
 
 function cacheFunction(cb) {
@@ -21,6 +26,14 @@ function cacheFunction(cb) {
   // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
+  let objec = {};
+  return function (x) {
+    if (!objec.hasOwnProperty(x)) {
+      objec[x] = cb(x);
+    }
+   return objec[x];
+  }
+  
 }
 
 // Bind
@@ -41,8 +54,8 @@ function getNombre(){
  // Escribir código, sin modificar lo que ya se encuentra escrito arriba, para poder llamar al método getNombre para obtener primero el nombre del instructor y luego para obtener el nombre del alumno.
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que bindear el this!
-let getNombreInstructor = undefined;
-let getNombreAlumno = undefined;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 
 /*Guardar en las siguientes tres variables una función que devuelva una cadena utilizando la función "crearCadena"
@@ -61,14 +74,14 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena){
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que usar bind para "bindear" algunos parámetros de la función crearCadena.
 
-let textoAsteriscos = undefined;
+let textoAsteriscos = crearCadena.bind(this,"*","*");
 
-let textoGuiones = undefined;
+let textoGuiones = crearCadena.bind(this,"-","-");
 
-let textoUnderscore = undefined;
+let textoUnderscore = crearCadena.bind(this,"_","_");
 
-
-
+var nuevo = 1;
+console.log(nuevo)
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
